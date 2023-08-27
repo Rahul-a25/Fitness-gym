727e5184cab93e25f79bbac25212d398db40b331
unknown <rahularora993676@gmail.com>
2023-08-28 00:47:35 +0530

    done

--- src/component/ApisFolder/AllData.jsx
+++ src/component/ApisFolder/AllData.jsx
@@ -3,34 +3,34 @@ import { AllData, FilterData } from "../Redux/action";
 
 
 const GetAllData= async(dispatch) =>{
-    axios.get('http://localhost:5000/results').then((res)=>{
-         dispatch(AllData(res.data))
-         dispatch(FilterData(res.data))
-        // SetAllData(res.data)
-        // SetFilterData(res.data)
+    // axios.get('http://localhost:5000/results').then((res)=>{
+    //      dispatch(AllData(res.data))
+    //      dispatch(FilterData(res.data))
+    //     // SetAllData(res.data)
+    //     // SetFilterData(res.data)
     
-    }).catch((err)=>{
-      alert('error')
-    })
+    // }).catch((err)=>{
+    //   alert('error')
+    // })
   
 
-    // const options = {
-    //   method: 'GET',
-    //   url: 'https://exercisedb.p.rapidapi.com/exercises',
-    //   headers: {
-    //     'X-RapidAPI-Key': 'f41c2f25b7mshbb2475314b3780ep1e9f08jsn34f500ddbe3f',
-    //     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    //   }
-    // };
+    const options = {
+      method: 'GET',
+      url: 'https://exercisedb.p.rapidapi.com/exercises',
+      headers: {
+        'X-RapidAPI-Key': 'f41c2f25b7mshbb2475314b3780ep1e9f08jsn34f500ddbe3f',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      }
+    };
     
-    // try {
-    //   const response = await axios.request(options);
-    //   console.log(response.data);
-    //   dispatch(AllData(response.data))
-    //   dispatch(FilterData(response.data))
-    // } catch (error) {
-    //   console.error(error);
-    // }
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      dispatch(AllData(response.data))
+      dispatch(FilterData(response.data))
+    } catch (error) {
+      console.error(error);
+    }
    
 }
 export default GetAllData
